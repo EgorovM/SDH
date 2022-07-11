a76f39428f689130ebfa62df7be085435946f720
EgorovM <egorov_michil@mail.ru>
2022-07-11 13:31:17 +0300

    [feat] mock translator

--- src/utils/multilanguage.py
+++ src/utils/multilanguage.py
@@ -1,7 +1,16 @@
+from collections import namedtuple
 from googletrans import Translator
 
 
-en_translator = Translator()
+moke_translate = namedtuple('Translate', ['text'])
+
+class MokeTranslator:
+    def translate(self, word: str) -> str:
+        return moke_translate(word)
+
+
+en_translator = MokeTranslator()
+
 
 def translate_word(word: str) -> str:
     return en_translator.translate(word).text
@@ -11,6 +20,6 @@ def en_vectorizer(vectorizer) -> None:
     keys = list(vectorizer.vocabulary_.keys())
 
     for key in keys:
-        key_en = en_translator.translate(key).text
+        key_en = translate_word(key)
         vectorizer.vocabulary_[key_en] = vectorizer.vocabulary_[key]
         del vectorizer.vocabulary_[key]
